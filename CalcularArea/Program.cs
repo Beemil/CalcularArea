15be4baba606ee4b983d02013b91559ef4741ecd
Beemil <valeskavr17@gmail.com>
2025-08-22 20:11:51 -0600

    Calculo de Area

--- CalcularArea/Program.cs
+++ CalcularArea/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using CalcularArea;
+
+Circulo circulo = new Circulo("Círculo", 7);
+Rectangulo rectangulo = new Rectangulo("Rectángulo", 8, 12);
+
+List<Forma> formas = new List<Forma>();
+
+formas.Add(circulo);
+formas.Add(rectangulo);
+
+foreach (var forma in formas)
+{
+    Console.WriteLine($"El área del {forma.Nombre} es: {forma.CalcularArea()}");
+}
